63a9058aa277111073b2d1aa4e0c6187f646da54
Matteo Muscas <thatman.j@gmail.com>
2019-06-19 14:28:40 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,11 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def self.search(name)
-    self.map {|s| s.name}
+  def self.search(name = 1)
+  if name == 1
+    self.all
+  else
+    self.all.find_all { |student| student.name.upcase.include? name.upcase }
   end
+end
 end

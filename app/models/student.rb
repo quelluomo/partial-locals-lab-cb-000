28e53d0424ef2e78f35ea273784d04aaf5d07e51
Matteo Muscas <thatman.j@gmail.com>
2019-06-19 14:05:16 +0000

    Automatically backed up by Learn

--- app/models/student.rb
+++ app/models/student.rb
@@ -14,7 +14,7 @@ class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
 
-  def search
+  def search(name)
     self.map {|s| s.name}
   end
 end
